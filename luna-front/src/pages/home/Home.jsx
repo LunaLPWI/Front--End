75fb188f13a53ecbe5f3e699d5be52919699acfc
ArthurV0n <arthur.pedroso@sptech.school>
2024-09-28 20:12:23 -0300

    o componente de formulário de pagamento está pronto mas precisa de teste com o back-end

--- luna-front/src/pages/home/Home.jsx
+++ luna-front/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import '../../global.css'; // Isso deve funcionar se você está no diretório AgendamentoServicos
 import Header from '../../components/Header/Header';
+import FormularioPagamento from '../../components/formularioPagamento/FormularioPagamento';
 
 function Home() {
   return (
     <div>
       <Header/>
       <h1>home!!!!!!!!</h1>
+      <FormularioPagamento/>
     </div>
   );
 }
