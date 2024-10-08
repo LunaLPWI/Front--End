4c93e9dae7d8ade78db3b8df6316a17688a58060
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-10-08 20:28:29 -0300

    feature: header dinamico e ajustes na home

--- luna-front/src/components/Footer/Footer.jsx
+++ luna-front/src/components/Footer/Footer.jsx
@@ -10,16 +10,14 @@ function Footer() {
                     <img src={domRoqueLogo} alt="logo da Dom Roque" className={styles.logo} />
                     <hr />
                     <nav className={styles.navLinks}>
-                        <a href="#">Sobre nós</a>
-                        <a href="#">Serviços</a>
-                        <a href="#">Funcionamento</a>
-                        <a href="#">Entrar</a>
+                        <a href="#sobre-nos">Sobre nós</a>
+                        <a href="#servicos">Serviços</a>
+                        <a href="#funcionamento">Funcionamento</a>
+                        <a href="/entrar">Entrar</a>
                     </nav>
                 </div>
                 <div className={styles.addressSection}>
-                    <a href="https://www.google.com/maps/dir//R.+Trist%C3%A3o+de+Alencar+Araripe+J%C3%BAnior,+388+-+Vila+Uniao+(Zona+Leste),+S%C3%A3o+Paulo+-+SP,+03272-060/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x94ce67f55408ddbb:0x90d40063b370f0e?sa=X&ved=1t:707&ictx=111">
-                        <img src={mapImage} alt="Mapa do local" className={styles.map} />
-                    </a>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.0493401313465!2d-46.518590624438836!3d-23.602563363107695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce67f55408ddbb%3A0x90d40063b370f0e!2sR.%20Trist%C3%A3o%20de%20Alencar%20Araripe%20J%C3%BAnior%2C%20388%20-%20Vila%20Uniao%20(Zona%20Leste)%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003272-060!5e0!3m2!1spt-BR!2sbr!4v1728429603542!5m2!1spt-BR!2sbr" width="280" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='Google Maps'></iframe>
                     <p>
                         R. Tristão de Alencar Araripe Júnior, 388<br />
                         Vila Uniao (Zona Leste), São Paulo - SP, 03272-060
