5906cfeceb89a1649f53a77899caaf421f28eb81
Luiz Fernando Meneses Pereira <luizfernandomdp14@outlook.com>
2024-10-21 11:00:00 -0300

    feature: tela agenda dos clientes e gerenciamento dos clientes pronta faltando apenas a ligação com o backend

---
+++ luna-front/src/components/Table/Table.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import style from './Table.module.css';
+import Filter from '../Filter/Filter';
+
+const TableHeader = ({ headers }) => {
+    return (
+        <thead>
+            <tr>
+                {headers.map((header, index) => (
+                    <th key={index}>{header}</th>
+                ))}
+            </tr>
+        </thead>
+    );
+};
+
+const TableBody = ({ data }) => {
+    return (
+        <tbody>
+            {data.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                    {row.map((cell, cellIndex) => (
+                        <td key={cellIndex} className={cellIndex === 2 ? style.dateCell : ''}>
+                            {cell}
+                        </td>
+                    ))}
+                </tr>
+            ))}
+        </tbody>
+    );
+};
+
+
+const DynamicTable = ({
+    headers,
+    data,
+    useFilter = false,
+    usePagination = false,
+    rowsPerPage = 10,
+}) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [filter, setFilter] = useState('');
+
+    const handleFilterChange = (value) => {
+        setFilter(value);
+        setCurrentPage(1);
+    };
+
+    const filteredData = data.filter((row) =>
+        row.some(cell => cell.toString().toLowerCase().includes(filter.toLowerCase()))
+    );
+
+    const dataToDisplay = filter ? filteredData : data;
+
+    const indexOfLastRow = currentPage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentRows = usePagination ? dataToDisplay.slice(indexOfFirstRow, indexOfLastRow) : dataToDisplay;
+
+    const totalPages = Math.ceil(dataToDisplay.length / rowsPerPage);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <div className={style.tableContainer}>
+            {useFilter && <Filter onFilterChange={handleFilterChange} />}
+
+            <table className={style.customTable}>
+                <TableHeader headers={headers} />
+                <TableBody data={currentRows} />
+            </table>
+
+            {usePagination && (
+                <div className={style.pagination}>
+                    <button
+                        onClick={() => paginate(currentPage - 1)}
+                        disabled={currentPage === 1}
+                    >
+                        Anterior
+                    </button>
+
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => paginate(index + 1)}
+                            className={currentPage === index + 1 ? style.active : ''}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+
+                    <button
+                        onClick={() => paginate(currentPage + 1)}
+                        disabled={currentPage === totalPages}
+                    >
+                        Próximo
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DynamicTable;
--- luna-front/src/routes.jsx
+++ luna-front/src/routes.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Cadastro from "./pages/CadastroUsuario/Cadastro";
+// import Cadastro from "./pages/CadastroUsuario/Cadastro";
 import Login from "./pages/Login/Login";
 import AgendamentoServicos from "./pages/AgendamentoServicos/AgendamentoServicos";
+import Agendamento from "./pages/Agendamento/Agendamento";
+import AgendaCliente from "./pages/AgendaCliente/AgendaCliente"
 import Perfil from "./pages/perfil/Perfil";
 import Planos from "./pages/planos/Planos";
+import GerenciamentoCliente from "./pages/GerenciamentoCliente/GerenciamentoCliente";
 
 export const Rotas = () => {
     return (
@@ -14,8 +17,11 @@ export const Rotas = () => {
                 <Route path="/planos" element={<Planos />} />
                 <Route path="/perfil" element={<Perfil />} />
                 <Route path="/agendamento-servicos" element={<AgendamentoServicos />} />
+                <Route path="/agendamentos" element={<Agendamento />} />
+                <Route path="/agenda-clientes" element={<AgendaCliente />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/cadastro" element={<Cadastro/>} />
+                {/* <Route path="/cadastro" element={<Cadastro/>} /> */}
+                <Route path="/gerenciamento-clientes" element={<GerenciamentoCliente/>} />
             </Routes>
         </BrowserRouter>
     );
